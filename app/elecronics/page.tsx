d0e3996006838677e7dd80128026c1283955ee76
devgrantjpg <devmodegrant@gmail.com>
2025-04-23 16:06:21 -0700

    update

--- app/elecronics/page.tsx
+++ app/elecronics/page.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 
-export default function Ecom() {
+export default function Electronics() {
   const styles = {
     navBar: {
       width: "100%",
