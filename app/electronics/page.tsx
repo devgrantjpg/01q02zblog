85ebf18504884b17a341928774319e851092c3ef
devgrantjpg <devmodegrant@gmail.com>
2025-04-23 16:32:37 -0700

    update

--- app/electronics/page.tsx
+++ app/electronics/page.tsx
@@ -149,13 +149,13 @@ export default function Electronics() {
           [
             { title: "Classic Radio", href: "", src: "/img/classicradio5.jpeg" },
             { title: "Classic Radio", href: "", src: "/img/classicradio6.jpeg" },
-            { title: "Computer Monitor", href: "", src: "" },
+            { title: "Classic Radio", href: "", src: "/img/retroRadiobrown" },
           ],
-          // [
-          //   { title: "Curved Computer Monitor", href: "", src: "/img/img2.jpeg" },
-          //   { title: "Computer Monitor", href: "", src: "" },
-          //   { title: "Computer Monitor", href: "", src: "" },
-          // ],
+          [
+            { title: "Classic Radio", href: "", src: "/img/victorlaRadio" },
+            { title: "Classic Radio", href: "", src: "/img/retroradioBackgroundblueCloseup" },
+            { title: "Curved Computer Monitor", href: "", src: "/img/img2.jpeg" },
+          ],
           // [
           //   { title: "Computer Monitor", href: "", src: "" },
           //   { title: "Computer Monitor", href: "", src: "" },
